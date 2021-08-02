aa9f7f08b9e45f8b87e481498880461d7b17ced8
Austin Long <46079987+alifelongwork@users.noreply.github.com>
2021-08-02 14:51:36 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,12 +53,4 @@ $(document).ready(function(){
 });
 })(jQuery)
 
-function openNav(){
-document.getElementById("ASideNav").style.width = "250px";
-document.getElementById("main").style.marginLeft ="250px";
-}
 
-function closeNav(){
-document.getElementById("ASideNav").style.width = "0";
-document.getElementById("main").style.width = "0";
-}
