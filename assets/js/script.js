9b4eeddcd3aeb130aa865dc0c8ae6f105e8d0160
Austin Long <46079987+alifelongwork@users.noreply.github.com>
2021-08-02 16:34:04 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
 
     
 (function($) {
-(function openNav(){
+$(function openNav(){
     document.getElementById("ASideNav").style.width = "250px";
     document.getElementById("container").style.marginLeft ="250px";
     }
 )
-function closeNav(){
+$(function closeNav(){
     document.getElementById("ASideNav").style.width = "0";
     document.getElementById("container").style.marginLeft = "0";
     }
+)
 $(document).ready(function(){
 
   // putting lines by the pre blocks
