a438c53aeeab97b2e64a7ec7d7e2e629ebea5a79
Austin Long <46079987+alifelongwork@users.noreply.github.com>
2021-08-02 14:44:40 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,14 +50,15 @@ $(document).ready(function(){
     $(window).scrollTop(0);
     return false;
   })
-  function openNav(){
-  document.getElementById("ASideNav").style.width = "250px";
-  document.getElementById("main").style.marginLeft ="250px";
-  }
-
-  function closeNav(){
-  document.getElementById("ASideNav").style.width = "0";
-  document.getElementById("main").style.width = "0";
-  }
 });
 })(jQuery)
+
+function openNav(){
+document.getElementById("ASideNav").style.width = "250px";
+document.getElementById("main").style.marginLeft ="250px";
+}
+
+function closeNav(){
+document.getElementById("ASideNav").style.width = "0";
+document.getElementById("main").style.width = "0";
+}
